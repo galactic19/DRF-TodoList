940f7ac520849b353a08c75c538e80ebfdaba066
galactic19 <galactic19@naver.com>
2022-06-08 00:37:21 +0900

    Todo모델 만들기

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DRF-TodoList
-DRF를 활용한 간단한 TodoList 만들어 보기
+# DRF를 활용한 간단한 TodoList 만들어 보기
+
+### commit - 1 
+    모델 생성하기
---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    creaetd = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    
