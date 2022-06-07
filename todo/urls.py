80cea4cf4bb9a2a5c222f7ac21b04a33062c3a59
galactic19 <galactic19@naver.com>
2022-06-08 01:43:30 +0900

    Todo 수정 API 만들기

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TodosAPIView, TodoGetAPIView, TodoCreateAPIView
+from .views import TodosAPIView, TodoGetAPIView, TodoCreateAPIView, TodoUpdateAPIView
 
 app_name = 'todo'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get/list/', TodosAPIView.as_view()),
     path('get/list/<int:pk>/', TodoGetAPIView.as_view()),
     path('post/list/', TodoCreateAPIView.as_view()),
+    path('put/list/<int:pk>/', TodoUpdateAPIView.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -36,3 +36,14 @@ class TodoCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class TodoUpdateAPIView(APIView):
+    def put(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        serializer = TodoCreateSerializer(todo, data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
